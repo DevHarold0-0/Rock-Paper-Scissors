5c37957f20575ad7917093eb95b36010a7e50b0a
harold <155407111+DevHarold0-0@users.noreply.github.com>
2024-04-03 02:12:10 -0400

    Complete rules for the round

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ let paper = 2;
 let scissors = 3;
 let randomNumber = Math.floor(Math.random() * 3) + 1; 
 
+
 function getComputerChoice() {
     switch (randomNumber) {
         case rock:
@@ -17,4 +18,36 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice());
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        console.log("It's a tie");
+    }
+
+    if (playerSelection === "Rock" && computerSelection === "Scissors") {
+        console.log(`You won! ${playerSelection} beats ${computerSelection}`);
+    }
+    else if (playerSelection === "Scissors" && computerSelection === "Rock") {
+        console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+    }
+
+    if (playerSelection === "Scissors" && computerSelection === "Paper") {
+        console.log(`You won! ${playerSelection} beats ${computerSelection}`);
+    }
+    else if (playerSelection === "Paper" && computerSelection === "Scissors") {
+        console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+    }
+
+    if (playerSelection === "Paper" && computerSelection === "Rock") {
+        console.log(`You won! ${playerSelection} beats ${computerSelection}`);
+    }
+    else if (playerSelection === "Rock" && computerSelection === "Paper") {
+        console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+    }
+}
+  
+
+
+  const playerSelection = "Paper";
+  const computerSelection = getComputerChoice();
+  console.log(playRound(playerSelection, computerSelection));
